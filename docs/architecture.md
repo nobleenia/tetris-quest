588be73d77ab80e5ea9450c588653e4340b2948e
jramoscl <joracle@protonmail.com>
2026-01-20 14:01:25 +0100

    tetris flash effect, version1

---
+++ docs/architecture.md
@@ -0,0 +1,45 @@
+## 🏗️ Project Architecture
+
+```
+src
+	├── js/
+	│	├── engine/
+	│	│	├── constants.js
+	│	│	├── controls.js
+	│	│	├── input.js
+	│	│	├── loop.js
+	│	│	└── state.js
+	│	│
+	│	├── game/
+	│	│	├── actions.js
+	│	│	├── board.js
+	│	│	├── dailyBest.js
+	│	│	├── harddrop.js
+	│	│	├── hold.js
+	│	│	├── lines.js
+	│	│	├── lives.js
+	│	│	├── lock.js
+	│	│	├── pieces.js
+	│	│	├── pressure.js
+	│	│	├── rotate.js
+	│	│	├── rules.js
+	│	│	├── score.js
+	│	│	└── spawn.js
+	│	│
+	│	├── ui/
+	│	│	├── dom.js
+	│	│	├── hud.js
+	│	│	├── pause.js
+	│	│	└── render.js
+	│	│
+	│	└── main.js
+	│
+	├── styles/
+	│	├── base.css
+	│	├── board.css
+	│	├── hud.css
+	│	└── overlay.css
+	│
+	└── index.html
+
+```
--- src/js/game/lines.js
+++ src/js/game/lines.js
@@ -1,45 +1,50 @@
 import { indexOf } from "./board.js";
+import { flashTetris } from "../ui/tetrisflash.js";
+
 
 // Returns number of cleared lines
 export function clearFullLines(state) {
-  const { cols, rows, lockedBoard } = state;
+	const { cols, rows, lockedBoard } = state;
 
-  let writeRow = rows - 1;
-  let cleared = 0;
+	let writeRow = rows - 1;
+	let cleared = 0;
 
-  // Scan from bottom up
-  for (let y = rows - 1; y >= 0; y--) {
-    let full = true;
+	// Scan from bottom up
+	for (let y = rows - 1; y >= 0; y--) {
+		let full = true;
 
-    for (let x = 0; x < cols; x++) {
-      if (lockedBoard[indexOf(x, y, cols)] === 0) {
-        full = false;
-        break;
-      }
-    }
+		for (let x = 0; x < cols; x++) {
+			if (lockedBoard[indexOf(x, y, cols)] === 0) {
+				full = false;
+				break;
+			}
+		}
 
-    if (full) {
-      cleared += 1;
-      continue; // skip copying this row (it disappears)
-    }
+		if (full) {
+			cleared += 1;
+			continue; // skip copying this row (it disappears)
+		}
 
-    // Copy this row down to writeRow (if needed)
-    if (writeRow !== y) {
-      for (let x = 0; x < cols; x++) {
-        lockedBoard[indexOf(x, writeRow, cols)] =
-          lockedBoard[indexOf(x, y, cols)];
-      }
-    }
+		// Copy this row down to writeRow (if needed)
+		if (writeRow !== y) {
+			for (let x = 0; x < cols; x++) {
+				lockedBoard[indexOf(x, writeRow, cols)] =
+				lockedBoard[indexOf(x, y, cols)];
+			}
+		}
+		writeRow -= 1;
+	}
 
-    writeRow -= 1;
-  }
+	if (cleared === 4) {
+		flashTetris();
+	}
 
-  // Fill remaining rows at the top with 0
-  for (let y = writeRow; y >= 0; y--) {
-    for (let x = 0; x < cols; x++) {
-      lockedBoard[indexOf(x, y, cols)] = 0;
-    }
-  }
+	// Fill remaining rows at the top with 0
+	for (let y = writeRow; y >= 0; y--) {
+		for (let x = 0; x < cols; x++) {
+			lockedBoard[indexOf(x, y, cols)] = 0;
+		}
+	}
 
-  return cleared;
+	return cleared;
 }
