cdd3a797c7c9e313dc16e3eea3c1069b39e96318
jramoscl <joracle@protonmail.com>
2026-01-21 12:36:13 +0100

    adding several 'themes/maps' to choose from

--- src/js/ui/stylemanager.js
+++ src/js/ui/stylemanager.js
@@ -1,9 +1,24 @@
 export function applyStyle(style) {
-	document.body.classList.remove("style-modern", "style-gameboy");
+	document.body.classList.remove(
+		"style-modern", 
+		"style-gameboy",
+		"style-neon",
+		"style-vaporwave"
+	);
 
-	if (style === "gameboy") {
-		document.body.classList.add("style-gameboy");
-	} else {
-		document.body.classList.add("style-modern");
+	switch (style) {
+		case "gameboy": 
+			document.body.classList.add("style-gameboy");
+			break;
+		case "neon": 
+			document.body.classList.add("style-neon");
+			break;	
+		case "vaporwave": 
+			document.body.classList.add("style-vaporwave");
+			break;
+		case "modern": 
+		default: 
+			document.body.classList.add("style-modern");
+			break;
 	}
 }
